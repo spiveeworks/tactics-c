926f77b551dd69f204def1c76b2c499bcd94ae8c
spiveeworks <davidm4vid@gmail.com>
2015-05-05 09:54:05 +1000

    Reboot Manual Upload dP3

--- src/head.h
+++
@@ -1,7 +0,0 @@
-
-
-void DrawBall(HDC hdc, RECT* prc);
-void UpdateBall(RECT* prc);
-
-LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow);
